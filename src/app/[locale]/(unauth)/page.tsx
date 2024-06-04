5bce7f3c598402b6e1f7dc73b8eb1adb9a173113
zapboy216 <zapboy216@gmail.com>
2024-06-04 07:47:25 -0400

    update json file add carousel

--- src/app/[locale]/(unauth)/page.tsx
+++ src/app/[locale]/(unauth)/page.tsx
@@ -1,11 +1,25 @@
 // app/page.tsx
+import { useTranslations } from 'next-intl';
 import React from 'react';
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from '@/components/ui/carousel';
+
+const t = useTranslations('Carousel');
 
 const HomePage: React.FC = () => {
   return (
     <main>
-      <p>Hello</p>
-      <p>Hello</p>
+      <Carousel orientation="vertical | horizontal">
+        <CarouselContent>
+          <CarouselItem>...</CarouselItem>
+          <CarouselItem>...</CarouselItem>
+          <CarouselItem>...</CarouselItem>
+        </CarouselContent>
+      </Carousel>
     </main>
   );
 };
