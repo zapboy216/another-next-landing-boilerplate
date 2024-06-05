d510bc480a87f6ac4508e283868891e88595885e
zapboy216 <zapboy216@gmail.com>
2024-06-05 12:22:17 -0400

    update basetemplate

--- src/app/[locale]/(unauth)/layout.tsx
+++ src/app/[locale]/(unauth)/layout.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { useTranslations } from 'next-intl';
+import React from 'react';
 
 import LocaleSwitcher from '@/components/LocaleSwitcher';
 import { BaseTemplate } from '@/templates/BaseTemplate';
--- src/templates/BaseTemplate.tsx
+++ src/templates/BaseTemplate.tsx
@@ -1,4 +1,5 @@
 import { useTranslations } from 'next-intl';
+import React from 'react';
 
 import { AppConfig } from '@/utils/AppConfig';
 
